839f0bf94fedf648d4abf40301e9f4fc9eeac038
Dispersora Digital <dispersora.digital@gmail.com>
2026-09-16 20:06:40 -0700

    Update entrypoint.sh
    Se actualiza la contraseña del dashboard de Hermes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ cat <<EOF > /root/.hermes/config.yaml
 dashboard:
   basic_auth:
     username: "admin"
-    password_hash: "NO_SE_MUESTRA_POR_SEGURIDAD"
+    password_hash: "pbkdf2:sha256:600000\$xK8jW2\$4c6198f16b23b8fbcad2dc8f4d99c35a8bc3e414163bb4beabf62804b9e4a8dc"
 
 llm:
   provider: "google"
